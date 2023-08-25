54ef27b92296c279634f4f03c681dff5ac93aa8b
Tom <tomzhongcn@gmail.com>
2023-08-25 20:50:30 +0930

    add cors and html page

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000; // You can choose any available port
 const crypto = require("crypto");
 
 const randomString = generateRandomString(5);
 
 app.get("/", (req, res) => {
-  res.send(randomString);
+  res.json({ message: randomString });
 });
 
 app.listen(port, () => {
