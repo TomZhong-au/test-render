3f1e2f82cd6a8c295b27530042bddde647b51e53
Tom <tomzhongcn@gmail.com>
2023-08-25 20:10:40 +0930

    minimum server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000; // You can choose any available port
+const crypto = require("crypto");
+
+const randomString = generateRandomString(5);
+
+app.get("/", (req, res) => {
+  res.send(randomString);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+function generateRandomString(length) {
+  const randomBytes = crypto.randomBytes(length);
+  const randomString = randomBytes.toString("base64url");
+  return randomString;
+}
